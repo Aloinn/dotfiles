6435abe03ebcfa866dfd292bf4751305d3eece19
Alain Nguyen <alainlam@amazon.com>
2025-06-14 01:32:16 -0700

    Updated nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,48 @@
-vim.g.mapleader = " "
--- views can only be fully collapsed with the global statusline
-vim.opt.laststatus = 3
--- Default splitting will cause your main splits to jump when opening an edgebar.
--- To prevent this, set `splitkeep` to either `screen` or `topline`.
-vim.opt.splitkeep = "screen"
-require("config.lazy")
-require("lazy").setup("plugins")
-require("options")
-require("keymaps")
+-- HELLO, welcome to NormalNvim!
+-- ---------------------------------------
+-- This is the entry point of your config.
+-- ---------------------------------------
+
+local function load_source(source)
+  local status_ok, error = pcall(require, source)
+  if not status_ok then
+    vim.api.nvim_echo(
+      {{"Failed to load " .. source .. "\n\n" .. error}}, true, {err = true}
+    )
+  end
+end
+
+local function load_sources(source_files)
+  vim.loader.enable()
+  for _, source in ipairs(source_files) do
+    load_source(source)
+  end
+end
+
+local function load_sources_async(source_files)
+  for _, source in ipairs(source_files) do
+    vim.defer_fn(function()
+      load_source(source)
+    end, 50)
+  end
+end
+
+local function load_colorscheme(colorscheme)
+    if vim.g.default_colorscheme then
+      if not pcall(vim.cmd.colorscheme, colorscheme) then
+        require("base.utils").notify(
+          "Error setting up colorscheme: " .. colorscheme,
+          vim.log.levels.ERROR
+        )
+      end
+    end
+end
+
+-- Call the functions defined above.
+load_sources({
+  "base.1-options",
+  "base.2-lazy",
+  "base.3-autocmds", -- critical stuff, don't change the execution order.
+})
+load_colorscheme("everforest")
+load_sources_async({ "base.4-mappings" })
--- .config/nvim/lua/options.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.nu = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
